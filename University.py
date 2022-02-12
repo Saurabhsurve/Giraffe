499f670b87fee13fe0736b23ce637da453618f3c
saurabh surve <sau.surve@gmail.com>
2022-02-12 14:46:30 +0100

    practice examples

---
+++ University.py
@@ -0,0 +1,8 @@
+class Student:
+
+    def __init__(self, v_name, v_email, v_age, v_country, v_is_current):
+        self.name = v_name
+        self.email = v_email
+        self.age = v_age
+        self.country = v_country
+        self.is_current= v_is_current
