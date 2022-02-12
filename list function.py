0c38ba89b09c05f997a35b4a439ed03b1c7e82a3
saurabh surve <sau.surve@gmail.com>
2022-02-12 15:23:54 +0100

    practice examples

--- list function.py
+++ list function.py
@@ -1,6 +1,6 @@
-lucky_numbers = [33,25, 27, 26,25, 37, 44]
-#lucky_numbers.sort()
-#lucky_numbers.reverse()
+lucky_numbers = [33, 25, 27, 26, 25, 37, 44]
+# lucky_numbers.sort()
+# lucky_numbers.reverse()
 lucky_numbers.remove(25)
 lucky_numbers.append(79)
 a = lucky_numbers.index(26)
