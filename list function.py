a84da596e42f3c35061a4ed09c30e1ef39bb7c1a
saurabh surve <sau.surve@gmail.com>
2022-02-13 21:25:01 +0100

    list functions updated

--- list function.py
+++ list function.py
@@ -1,8 +1,15 @@
 lucky_numbers = [33, 25, 27, 26, 25, 37, 44]
-# lucky_numbers.sort()
-# lucky_numbers.reverse()
+lucky_numbers.sort()
+print(lucky_numbers)
+lucky_numbers = [33, 25, 27, 26, 25, 37, 44]
+lucky_numbers.reverse()
+print(lucky_numbers)
+
 lucky_numbers.remove(25)
+print(lucky_numbers)
+
 lucky_numbers.append(79)
+print(lucky_numbers)
 a = lucky_numbers.index(26)
 print(lucky_numbers)
 print(a)
